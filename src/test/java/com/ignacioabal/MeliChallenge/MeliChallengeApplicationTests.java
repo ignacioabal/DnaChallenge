09c1a7cf35b8b37b09143a4b46f01cc707e9e317
ignacioabal <ignacioleonardoabal@gmail.com>
2021-04-23 20:32:46 -0300

    First commit

---
+++ src/test/java/com/ignacioabal/MeliChallenge/MeliChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignacioabal.MeliChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
