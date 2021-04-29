faa9c34119795afeac000c11019b2897f81d9864
Ignacio Abal <iaba@guidewire.com>
2021-04-29 19:03:32 -0300

    Refactored DnaIterator and added dna validation on DnaAnalyzer.

--- src/main/java/com/ignacioabal/MeliChallenge/DnaAnalyzer/DnaAnalyzer.java
+++ src/main/java/com/ignacioabal/MeliChallenge/DnaAnalyzer/DnaAnalyzer.java
@@ -1,31 +1,27 @@
 package com.ignacioabal.MeliChallenge.DnaAnalyzer;
 
-import com.ignacioabal.MeliChallenge.DnaIterator.DnaColumnIterator;
-import com.ignacioabal.MeliChallenge.DnaIterator.DnaDiagonalIterator;
 import com.ignacioabal.MeliChallenge.DnaIterator.DnaIterator;
-import com.ignacioabal.MeliChallenge.DnaIterator.DnaRowIterator;
 
 public class DnaAnalyzer {
-    private DnaIterator RowIterator;
-    private DnaIterator ColumnIterator;
-    private DnaIterator DiagonalIterator;
 
-    public DnaAnalyzer(){
-        InstantiateIterators();
-    }
-
-    public boolean isMutant(String[] dna){
-        int totalMutantSequences = 0;
-        totalMutantSequences += RowIterator.findMutantSequences(dna);
-        totalMutantSequences += ColumnIterator.findMutantSequences(dna);
-        totalMutantSequences += DiagonalIterator.findMutantSequences(dna);
-
+    public static boolean isMutant(String[] dna){
+        DnaIterator dnaIterator = new DnaIterator(dna);
+        int totalMutantSequences = dnaIterator.findMutantSequences();
         return totalMutantSequences > 2;
     }
 
-    private void InstantiateIterators(){
-        this.RowIterator = new DnaRowIterator();
-        this.ColumnIterator = new DnaColumnIterator();
-        this.DiagonalIterator = new DnaDiagonalIterator();
+    public static boolean isDnaValidForAnalysis(String[] dna){
+        if(dna == null) return false;
+        return areAllDnaRowsSameLength(dna);
+    }
+
+    private static boolean areAllDnaRowsSameLength(String[] dna){
+        int firstRowLength = dna[0].length();
+
+        for(String dnaString: dna){
+            if(dnaString.length() != firstRowLength) return false;
+        }
+
+        return true;
     }
 }
